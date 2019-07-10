759d0e37afc16a3df0a649004b6c687225f25d75
Hakeem Shrief <abalsh@ttu.ee>
2019-07-10 20:40:23 +0400

    Fetch from Accuweather City API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,16 @@
-const key = TDB8WECySoaj6M0Rkpvai8F4zT8CfsdR;
+const key = 'TDB8WECySoaj6M0Rkpvai8F4zT8CfsdR';
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+    // returns a promise
+};
+// process promise
+getCity('dubai')
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
