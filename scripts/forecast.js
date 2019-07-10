0b63c3d7ad42f8efc93963224ba3831d4cac868c
Hakeem Shrief <abalsh@ttu.ee>
2019-07-10 20:51:25 +0400

    Fetch from Accuweather Current Conditions API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,16 @@
 const key = 'TDB8WECySoaj6M0Rkpvai8F4zT8CfsdR';
 
+const getWeather = async(id) => {
+    
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+// Get city Info
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -7,10 +18,5 @@ const getCity = async (city) => {
     const response = await fetch(base + query);
     const data = await response.json();
 
-    return data[0];
-    // returns a promise
+    return data[0]; // returns a promise
 };
-// process promise
-getCity('dubai')
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
